9fe91f0d07ae9ec50ed91ce687ae55b9a0840bfa
cook <yuanxiaojie6616@163.com>
2012-06-09 00:01:38 +0800

    + ‘Codes/sort.c’

---
+++ Codes/sort.c
@@ -0,0 +1,8 @@
+/* sort the elements of an array from largest to smallest */
+#include <stdio.h>
+main()
+{
+	printf("Input several numbers you want to sort(enter to the end): ");
+	double dval;
+	while (scanf("%lf", &dval))
+}
