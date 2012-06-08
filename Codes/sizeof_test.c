b659e4a69f4937a38239804b991b3601992208a8
cook <yuanxiaojie6616@163.com>
2012-06-08 19:56:53 +0800

    / ‘Codes/sizeof_test.c’

--- Codes/sizeof_test.c
+++ Codes/sizeof_test.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 main()
 {
+	short s;
 	int i;
 	long l;
 	float f;
 	double d;
 	char c;
-	printf(" type\tsize\n int\t%2d\n long\t%2d\n float\t%2d\n double\t%2d\n char\t%2d\n", sizeof(i), sizeof(l), sizeof(f), sizeof(d), sizeof(c));
+	printf(" type\t\tsize\n short\t\t%2d\n int\t\t%2d\n long\t\t%2d\n float\t\t%2d\n double\t\t%2d\n char\t\t%2d\n", sizeof(s), sizeof(i), sizeof(l), sizeof(f), sizeof(d), sizeof(c));
 }
