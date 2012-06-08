93d04f1076930763fb05d2c027950d6da39dc49d
cook <yuanxiaojie6616@163.com>
2012-06-09 00:49:29 +0800

    / ‘Codes/sort.c’

--- Codes/sort.c
+++ Codes/sort.c
@@ -1,8 +1,28 @@
 /* sort the elements of an array from largest to smallest */
 #include <stdio.h>
+// #include <stdlib.h>
 main()
 {
-	printf("Input several numbers you want to sort(enter to the end): ");
-	double dval;
-	while (scanf("%lf", &dval))
+	int *arr;
+	int size;
+	printf("Enter the number of integers you want to sort: ");
+	scanf("%d", &size);
+	arr = (int*)malloc(size*sizeof(int));
+
+	int i, j, max, max_index, tmp;
+	for (i = 0; i != size; ++i) {
+		for (j = i+1; j != size; ++j) {
+			max = arr[i];
+			max_index = i;
+			if (max < arr[j]) {
+				max = arr[j];
+				max_index = j;
+			}
+		tmp = arr[i];
+		arr[i] = max;
+		arr[max_index] = tmp;
+		}
+	}
+	free(arr);
+
 }
