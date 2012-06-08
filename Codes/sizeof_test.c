93d5e9e3f9c618508d86855afe46dd3c1daec9f8
cook <yuanxiaojie6616@163.com>
2012-06-08 19:53:03 +0800

    + ‘Codes/sizeof_test.c’

---
+++ Codes/sizeof_test.c
@@ -0,0 +1,11 @@
+/* output the bit size of different data types */
+#include <stdioh>
+main()
+{
+	int i;
+	long l;
+	float f;
+	double d;
+	char c;
+	printf("type\tsize\n int\t%d\n long\t%d\n float\t%d\n double\t%d\n char\t%d\n", sizeof(i), sizeof(l), sizeof(f), sizeof(d), sizeof(c));
+}
