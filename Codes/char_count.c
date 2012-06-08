d7ce850dffede23dd0b257d5bd029df389949490
cook <yuanxiaojie6616@163.com>
2012-06-08 20:06:49 +0800

    + ‘Codes/char_count.c’

---
+++ Codes/char_count.c
@@ -0,0 +1,10 @@
+/* count the number of characters in the input string */
+#include <stdio.h>
+main()
+{
+	int cnt = 0;
+	printf("Input a string(put enter to the end): \n");
+	while(getchar() != '\n')
+		cnt++;
+	printf("%d", cnt);
+}
