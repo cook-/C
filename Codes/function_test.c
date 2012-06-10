2756112b60ca581c1acb1d528b75ecc2c2823e58
cook <yuanxiaojie6616@163.com>
2012-06-11 01:00:30 +0800

    / ‘Codes/a.out’ / ‘Codes/function_test.c’

--- Codes/function_test.c
+++ Codes/function_test.c
@@ -2,27 +2,25 @@
 #include <stdio.h>
 
 long factorial(int);
-long power(int, int);
+int power(int, int);
 
 main()
 {
 	int base, exp;
-	puts("Input two integers. \n");
-	puts("The 1st one: \n");
+	puts("Input two integers.");
+	puts("The 1st one:");
 	scanf("%d", &base);
-	putchar('\n');
-	puts("The 2nd one: \n");
-	sacnf("%d", &exp);
-	putchar('\n');
-	printf("(%d^%d)! = %ld", base, exp, factorial(power(base, exp)));
+	puts("The 2nd one:");
+	scanf("%d", &exp);
+	printf("(%d^%d)! = %ld\n", base, exp, factorial(power(base, exp)));
 }
 
 long factorial(int n)
 {
-	return n=0 ? 1 : n*factoria(n-1);
+	return (n == 0) ? 1 : n*factorial(n-1);
 }
 
-long power(int base, int exp)
+int power(int base, int exp)
 {
 	int i, result = 1;
 	for (i = 0; i != exp; ++i)
