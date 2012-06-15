ad5cc2dcbe22ed773dd8b0bc2b388912e8ee9737
cook <yuanxiaojie6616@163.com>
2012-06-15 17:04:05 +0800

    + ‘Codes/static_test.c’

---
+++ Codes/static_test.c
@@ -0,0 +1,18 @@
+/* Use static variable to calculate 1!, 2!, ..., n! */
+#include <stdio.h>
+int fac(n)
+{
+	static result = 1;
+	return result*n;
+}
+
+main()
+{
+	int n;
+	printf("Input an integer: \n");
+	scanf("%d", n);
+	int i;
+	for (i = 0; i != n; ++i) {
+		printf("%d!=%d\n", i, fac(i));
+	}
+}
