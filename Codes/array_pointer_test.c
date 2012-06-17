05f0bf286cdba0c994ac207413cf4e47441beccb
cook <yuanxiaojie6616@163.com>
2012-06-17 23:07:20 +0800

    / ‘Codes/array_pointer_test.c’

--- Codes/array_pointer_test.c
+++ Codes/array_pointer_test.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 main()
 {
-	int arr[10], i;
+	int arr[10], *ptr, i;
+
+	puts("Method 1: \n");
 	for (i = 0; i != 10; ++i)
 		*(arr + i) = i;
 	for (i = 0; i != 10; ++i)
 		printf("a[%d] = %d\n", i, *(arr + i));
+
+	puts("Method 2: \n");
+	ptr = arr;
+	for (i = 0; i != 10; ++i)
+		*ptr++ = i;
+	for (i = 0; i != 10; ++i)
+		printf("a[%d] = %d\n", i, *ptr++);
 }
