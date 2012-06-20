de201e3ba25a4bacb8384fa8fb182a1b6ac5cf5b
cook <yuanxiaojie6616@163.com>
2012-06-20 18:39:10 +0800

    + ‘Codes/2d_array_test.c’

---
+++ Codes/2d_array_test.c
@@ -0,0 +1,27 @@
+/* print the address& value of 2d-array elements */
+main()
+{
+	printf("%d,", a);
+	printf("%d,", *a);
+	printf("%d,", a[0]);
+	printf("%d,", &a[0]);
+	printf("%d\n", &a[0][0]);
+	printf("%d,", a + 1);
+	printf("%d,", *(a + 1));
+	printf("%d,", a[1]);
+	printf("%d,", &a[1]);
+	printf("%d\n", &a[1][0]);
+	printf("%d,", a + 2);
+	printf("%d,", *(a + 2));
+	printf("%d,", a[2]);
+	printf("%d,", &a[2]);
+	printf("%d\n", &a[2][0]);
+	printf("%d,", a[1] + 1);
+	printf("%d\n", *(a + 1) + 1);
+	printf("%d,", *(a[1] + 1));
+	printf("%d,\n", *(*(a + 1) + 1));
+	printf("%d\n", );
+	printf("%d\n", );
+	printf("%d\n", );
+	printf("%d\n", );
+}
