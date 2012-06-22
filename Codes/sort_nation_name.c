9879cc86c71f7db28c2e335ab5e28a34c1fbfe37
cook <yuanxiaojie6616@163.com>
2012-06-23 03:14:11 +0800

    + ‘Codes/sort_nation_name.c’

---
+++ Codes/sort_nation_name.c
@@ -0,0 +1,35 @@
+/* sort nation names in alphabetical order */
+#include <stdio.h>
+void sort(char**, int);
+void print(char**, int);
+
+main
+{
+	static char *name[] = { "CHINA", "AMERICA", "AUSTRALIA", 
+							"FRANCE", "GERMAN" };
+	sort(name, 5);
+	print(name, 5);
+}
+
+void sort(char *name[], int n)
+{
+	int i, j, min_index, *tmp;
+	for (i = 0; i != n - 1; ++i) {
+		min_index = i;
+		for (j = i + 1; j != n; ++j) {
+			if (strcmp(*(name + min_index), *(name + j)))
+				min_index = j;
+		}
+		if (min_index != i)
+			tmp = *(name + i);
+			*(name + i) = *(name + min_index);
+			*(name + min_index) = tmp;
+	}
+}
+
+void print(char *name[], int n)
+{
+	int i;
+	for (i = 0; i != n; ++i) 
+		printf("%s\n", *(name + i));
+}
