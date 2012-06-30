aee881c792852059342c56c35f468205329a4b49
cook <yuanxiaojie6616@163.com>
2012-06-30 22:11:19 +0800

    + ‘Codes/struct_test.c’

---
+++ Codes/struct_test.c
@@ -0,0 +1,29 @@
+/*
+ *	Create a phone memo using array of structs.
+ */
+
+#include <stdio.h>
+#define NUM	3
+struct member{
+	char name[10];
+	char phone_num[15];
+	}
+
+main()
+{
+	enum rank {1st, 2nd, 3rd, 4th, 5th} nth = 1st;
+//	char *nth[5] = {"1st", "2nd", "3rd", "4th", "5th"};
+	struct member memo[NUM];
+	int i;
+	for (i = 0; i != NUM; ++i) {
+		printf("Enter %s name: ", nth++);
+		scanf("%s", &memo[i].member.name);
+		printf("\ \ phone number: ");
+		sacnf("%s", &memo[i].member.phone_num);
+	}
+
+	puts("\n\t\t\tTelephone Memo");
+	printf("Name\t\tPhone\n");
+	for (i = 0; i != NUM; ++i)
+		printf("%s\t%s\n", memo[i].member.name, memo[i].member.phpne_num);
+}
